ebda0fed28f1a58416d5172adcceceec4c737969
jfoster17 <jonathan.bruce.foster@gmail.com>
2015-02-02 18:30:00 -0500

    Start adding unit tests

---
+++ test_on_synthetic.py
@@ -0,0 +1,13 @@
+from snythetic_spectrum import SyntheticSpectrum
+import moments
+import clean_spectrum
+
+def test_mom0():
+    a = SnytheticSpectrum()
+    test_spectrum = a.generate_spectrum()
+    noise_free_mom0 = a.calculate_integrated_intensity()
+    print(noise_free_mom0)
+    cleaned_spectrum = clean_spectrum.baseline_and_deglitch(test_spectrum)
+    
+    assert 
+    
