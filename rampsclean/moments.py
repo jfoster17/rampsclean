22f06e8c4f5a6081a1b49be42f3ae9d3875e9c77
jfoster17 <jonathan.bruce.foster@gmail.com>
2015-02-03 13:52:46 -0500

    Moment-masking now dilates/erodes and add more tests

--- rampsclean/moments.py
+++ rampsclean/moments.py
@@ -8,15 +8,29 @@ Moment 0 (integrated intensity) and error
 import numpy as np
 import numpy.ma as ma
 import clean_spectrum
+from scipy import ndimage
 
-def identify_signal_estimate_noise(input_spectrum,ww=20):
+def identify_signal_estimate_noise(input_spectrum,do_expansion=True,ww=20):
     """
-    Use the local-standard-deviation to mask signal
+    Use the local-standard-deviation to identify signal
     
+    Calculate the local-standard-deviation (y-array) for
+    an input spectrum, estimate the noise, and then return
+    a masked spectrum with just the high-signal part kept.
+    
+    Optionally apply binary erosion and dilation to the mask
+    in order to remove noise channels and to expand real
+    signal channels down to a lower level. Generally this 
+    should improve the fidelity of singal recovery.
     """
     y = clean_spectrum.make_local_stddev(input_spectrum,ww=ww)
     k_est = np.median(y)
     signal_spec = clean_spectrum.mask_spectrum(y,ww,input_spectrum,keep_signal=True)
+    if do_expansion:
+        basic_mask = signal_spec.mask
+        eroded_mask = ndimage.binary_erosion(basic_mask,iterations=2)
+        dilated_mask = ndimage.binary_dilation(eroded_mask,iterations=5)
+        signal_spec.mask = dilated_mask
     return(signal_spec,k_est)
 
 
--- tests/test_on_synthetic.py
+++ tests/test_on_synthetic.py
@@ -2,17 +2,70 @@ import rampsclean.synthetic_spectrum as snythetic_spectrum
 import rampsclean.moments as moments
 import rampsclean.clean_spectrum as clean_spectrum
 import numpy.ma as ma
+import numpy as np
 
-def test_mom0():
-    a = snythetic_spectrum.SyntheticSpectrum()
+def check_mom0(a):
+    """
+    Run full process on a test spectrum and compare integrated intensity
+    """
     test_spectrum = a.generate_spectrum()
     noise_free_mom0,noise_free_mom0_err = a.calculate_integrated_intensity()
-    print(noise_free_mom0)
+    #print(noise_free_mom0)
     cleaned_spectrum = clean_spectrum.baseline_and_deglitch(test_spectrum)
     signal_spectrum,noise_estimate = moments.identify_signal_estimate_noise(cleaned_spectrum)
-    print(ma.count(signal_spectrum))
-    cleaned_mom0,cleaned_mom0_err = moments.get_integrated_intensity(signal_spectrum,noise_estimate,downsample_fact=7)
-    print(cleaned_mom0)
-    print(cleaned_mom0_err)
+    cleaned_mom0,cleaned_mom0_err = moments.get_integrated_intensity(
+                                    signal_spectrum,noise_estimate,downsample_fact=7)
     assert abs(cleaned_mom0-noise_free_mom0) < 3*cleaned_mom0_err
     
+
+def test_mom0_highSNR():
+    parameters = {
+        "spec_length" : 16384, #Spectrum properties
+        "noise_level" : 0.02, #Noise properties
+        "baseline_poly_order" : 2,  #Baseline properties
+        "baseline_poly_params" : np.array([-0.1,+1e-6,-5e-10,+1e-13]),
+        "do_random_baseline" : False,
+        "nh3_amplitude" : 4.0, #NH3 spectrum properties
+        "nh3_width" : 40.,
+        "nh3_position" : 2000.,
+        "nh3_offset" : 300.,
+        "num_spikes" : 10.,
+        "spikes_amp"  : 4.,
+    }
+    a = snythetic_spectrum.SyntheticSpectrum(parameters=parameters)
+    check_mom0(a)
+    
+def test_mom0_lowSNR():
+    parameters = {
+        "spec_length" : 16384, #Spectrum properties
+        "noise_level" : 0.50, #Noise properties
+        "baseline_poly_order" : 2,  #Baseline properties
+        "baseline_poly_params" : np.array([-0.1,+1e-6,-5e-10,+1e-13]),
+        "do_random_baseline" : False,
+        "nh3_amplitude" : 3.0, #NH3 spectrum properties
+        "nh3_width" : 50.,
+        "nh3_position" : 4000.,
+        "nh3_offset" : 300.,
+        "num_spikes" : 10.,
+        "spikes_amp"  : 4.,
+    }
+    
+    a = snythetic_spectrum.SyntheticSpectrum(parameters=parameters)
+    check_mom0(a)
+    
+def test_mom0_broadlines():
+    parameters = {
+        "spec_length" : 16384, #Spectrum properties
+        "noise_level" : 0.2, #Noise properties
+        "baseline_poly_order" : 2,  #Baseline properties
+        "baseline_poly_params" : np.array([-0.1,+1e-6,-5e-10,+1e-13]),
+        "do_random_baseline" : False,
+        "nh3_amplitude" : 3.0, #NH3 spectrum properties
+        "nh3_width" : 100.,
+        "nh3_position" : 2000.,
+        "nh3_offset" : 300.,
+        "num_spikes" : 10.,
+        "spikes_amp"  : 4.,
+    }
+    a = snythetic_spectrum.SyntheticSpectrum(parameters=parameters)
+    check_mom0(a)
