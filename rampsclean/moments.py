eed5600d40ba481dc14fffcdc9ff7786c92efaeb
jfoster17 <jonathan.bruce.foster@gmail.com>
2015-02-03 13:09:36 -0500

    Full-process test now works on default high SNR spectrum

--- rampsclean/moments.py
+++ rampsclean/moments.py
@@ -7,8 +7,20 @@ Moment 0 (integrated intensity) and error
 """
 import numpy as np
 import numpy.ma as ma
+import clean_spectrum
 
-def get_integrated_intensity(input_spectrum,noise_estimate):
+def identify_signal_estimate_noise(input_spectrum,ww=20):
+    """
+    Use the local-standard-deviation to mask signal
+    
+    """
+    y = clean_spectrum.make_local_stddev(input_spectrum,ww=ww)
+    k_est = np.median(y)
+    signal_spec = clean_spectrum.mask_spectrum(y,ww,input_spectrum,keep_signal=True)
+    return(signal_spec,k_est)
+
+
+def get_integrated_intensity(input_spectrum,noise_estimate,downsample_fact=1.):
     """
     Calculate the integrated intensity.
     
@@ -20,8 +32,12 @@ def get_integrated_intensity(input_spectrum,noise_estimate):
     with a pre-calculated noise_estimate, as one generally
     cannot calculate the noise in the spectrum from the
     signal-only portion of the spectrum.
+    
+    downsample_fact = amount by which the spectrum has been
+                      downsampled (since mom0 here is in
+                      units of channels).
     """
     mom0 = ma.sum(input_spectrum)
     num_channels = ma.count(input_spectrum)
     mom0_err = np.sqrt(num_channels)*noise_estimate
-    return(mom0,mom0_err)
+    return(mom0*downsample_fact,mom0_err*downsample_fact)
--- tests/test_on_synthetic.py
+++ tests/test_on_synthetic.py
@@ -1,13 +1,18 @@
 import rampsclean.synthetic_spectrum as snythetic_spectrum
-import rampsclean.moments
+import rampsclean.moments as moments
 import rampsclean.clean_spectrum as clean_spectrum
+import numpy.ma as ma
 
 def test_mom0():
     a = snythetic_spectrum.SyntheticSpectrum()
     test_spectrum = a.generate_spectrum()
-    noise_free_mom0 = a.calculate_integrated_intensity()
+    noise_free_mom0,noise_free_mom0_err = a.calculate_integrated_intensity()
     print(noise_free_mom0)
     cleaned_spectrum = clean_spectrum.baseline_and_deglitch(test_spectrum)
-    
-    assert 3*5 == 15
+    signal_spectrum,noise_estimate = moments.identify_signal_estimate_noise(cleaned_spectrum)
+    print(ma.count(signal_spectrum))
+    cleaned_mom0,cleaned_mom0_err = moments.get_integrated_intensity(signal_spectrum,noise_estimate,downsample_fact=7)
+    print(cleaned_mom0)
+    print(cleaned_mom0_err)
+    assert abs(cleaned_mom0-noise_free_mom0) < 3*cleaned_mom0_err
     
